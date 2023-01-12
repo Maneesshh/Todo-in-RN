a3c9eff0a8f1833e8ed870330efd1bbeafdfeb8d
Maneesshh <manisbhattrai@gmail.com>
2023-01-12 20:06:54 +0545

    Before navigation Screen

--- App.js
+++ App.js
@@ -1,51 +1,55 @@
-import {StyleSheet, View,Text} from 'react-native';
-const PlaceholderImage = require('./assets/Images/ggg.jpg');
-import ImageViewer from './Components/ImageViewer';
+import { StyleSheet, View, Text } from "react-native";
+
+const PlaceholderImage = require("./assets/Images/ggg.jpg");
+import ImageViewer from "./Components/ImageViewer";
 import Todo from "./Components/Todo";
 import Rct from "./Components/RCt";
 import Hooksform from "./Components/Hooksform";
 import Reg from "./Components/Reg";
 import React from "react";
 import Login from "./Components/login";
+import { NavigationContainer } from "@react-navigation/native";
+
 export default function App() {
-    return (
-        <View style={styles.container}>
-               <View style={styles.imageContainer}>
-                   {/*    <ImageViewer placeholderImageSource={PlaceholderImage} />*/}
-            {/*</View>*/}
-            {/* <View style={styles.footer}>*/}
-            {/* /!*<Button theme="primary" title="Click Me Please !"/>*!/*/}
-            {/*   </View>*/}
-            {/*<Todo/>*/}
-            <Reg/>
-               </View>
-            <View style={styles.view2}>
-                <Login/>
-            </View>
-            {/*<Hooksform/>*/}
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          {/*    <ImageViewer placeholderImageSource={PlaceholderImage} />*/}
+          {/*</View>*/}
+          {/* <View style={styles.footer}>*/}
+          {/* /!*<Button theme="primary" title="Click Me Please !"/>*!/*/}
+          {/*   </View>*/}
+          {/*<Todo/>*/}
+          <Reg />
         </View>
-    );
+        <View style={styles.view2}>
+          <Login />
+        </View>
+        {/*<Hooksform/>*/}
+      </View>
+    </NavigationContainer>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    imageContainer: {
-        flex: 1,
-        paddingTop: 58,
-    },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    },
-    view2:{
-        flex:1,
-    },
-    footer:{
-        alignItems:"center",
-        flex:0.5,
-    },
-
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  view2: {
+    flex: 1,
+  },
+  footer: {
+    alignItems: "center",
+    flex: 0.5,
+  },
 });
--- Components/login.js
+++ Components/login.js
@@ -1,67 +1,103 @@
 import React from "react";
-import {View, StyleSheet, Text, TextInput, Button} from "react-native";
-import {Controller, useForm} from "react-hook-form";
-export default ()=>{
-    const{handleSubmit,control,formState:{errors}}= useForm({
-        defaultValues:{
-            phone:'',
-            password:'',
-            rememberMe:false,
-        }
-    })
-    const onsubmit=data=>{
-        alert(data.phone+" "+data.password+" "+data.rememberMe);
-    }
-    return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Login</Text>
-            <Text style={styles.label}>Phone or Email</Text>
-            <Controller render={({field:{onChange,onBlur,value}})=>(
-                <TextInput onChangeText={value=>onChange(value)} value={value} onBlur={onBlur} style={styles.input} />
-            )} name="phone" control={control} rules={{required:true}} defaultValue='' />
-            {errors.phone && <Text style={styles.error}>Email or Phone required</Text> }
-            <Text style={styles.label} >Password</Text>
-            <Controller render={({field:{onChange,onBlur,value}})=>(
-                <TextInput onChangeText={value=>onChange(value)} value={value} onBlur={onBlur} style={styles.input} />
-            )} name="password" control={control} rules={{required:true}} defaultValue='' />
-            {errors.password && <Text style={styles.error}>Password is required</Text> }
-            {/*<Controller render={({field:{onChange,value}})=>(*/}
-            {/*    <CheckBox onChange={value=>onChange(value)} checked={value}>Remember Me</CheckBox>*/}
-            {/*)} name="rememberMe" control={control} />*/}
-            <Button title="Sign In" onPress={handleSubmit(onsubmit)} />
-        </View>
-    );
-}
-const styles=StyleSheet.create({
-    container:{
-        height:'50%',
-        width:'80%',
-        padding:6,
-        marginTop:90,
-        marginLeft:40,
-        flex:3,
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  phone: yup.string().email().required(),
+  password: yup.string().min(8).max(32).required(),
+});
+export default () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      phone: "",
+      password: "",
     },
-    label:{
-        color:'#000',
-        fontWeight:"400",
-        marginBottom:10,
-        fontSize:14,
-    },
-    input:{
-        borderWidth:1,
-        borderColor:'#000',
-        borderStyle:"solid",
-        borderRadius:1,
-        marginBottom:5,
-    },
-    text:{
-        fontWeight:"bold",
-        fontSize:24,
-        color:'#000',
-        marginLeft:'38%'
-    },
-    error:{
-        color:"red",
-        fontWeight:"bold",
-    }
+    resolver: yupResolver(schema),
+  });
+  const onsubmit = (data) => {
+    alert(data.phone + " " + data.password);
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Login</Text>
+      <Text style={styles.label}>Phone or Email</Text>
+      <Controller
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            onChangeText={(value) => onChange(value)}
+            value={value}
+            onBlur={onBlur}
+            style={styles.input}
+          />
+        )}
+        name="phone"
+        control={control}
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      {errors.phone && (
+        <Text style={styles.error}>Email or Phone required</Text>
+      )}
+      <Text style={styles.label}>Password</Text>
+      <Controller
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            onChangeText={(value) => onChange(value)}
+            value={value}
+            onBlur={onBlur}
+            style={styles.input}
+          />
+        )}
+        name="password"
+        control={control}
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      {errors.password && (
+        <Text style={styles.error}>
+          Minimum 8 character Password is required
+        </Text>
+      )}
+      <Button title="Sign In" onPress={handleSubmit(onsubmit)} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    height: "50%",
+    width: "80%",
+    padding: 6,
+    marginTop: 90,
+    marginLeft: 40,
+    flex: 3,
+  },
+  label: {
+    color: "#000",
+    fontWeight: "400",
+    marginBottom: 10,
+    fontSize: 14,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000",
+    borderStyle: "solid",
+    borderRadius: 1,
+    marginBottom: 5,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#000",
+    marginLeft: "38%",
+  },
+  error: {
+    color: "red",
+    fontWeight: "bold",
+  },
 });
